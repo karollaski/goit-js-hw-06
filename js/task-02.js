f59abfa6e849c6a17f67c3668453ca345b2863bd
Karol Łaski <karol.laski994@gmail.com>
2023-02-18 19:43:20 +0100

    finished task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,22 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+for (const ingredient of ingredients) {
+  const name = ingredient;
+
+  const elements = document.createElement("li");
+  console.log(elements);
+  elements.classList.add("item");
+  console.log(elements);
+  elements.textContent = name;
+  console.log(elements);
+
+  const list = document.querySelector("#ingredients");
+  list.append(elements);
+}
