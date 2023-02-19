1261b77bf60a1555ab9667a9c0839fff7bf9a140
Karol Łaski <karol.laski994@gmail.com>
2023-02-19 11:15:16 +0100

    finished task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector(".login-form");
 
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+  const objectForm = {
+    email: email.value,
+    password: password.value,
+  };
+  event.currentTarget.reset();
+};
+
+form.addEventListener("submit", handleSubmit);
