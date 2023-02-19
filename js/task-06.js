5fd3e7ef8e1c1647feddebf479ea626c731c0893
Karol Łaski <karol.laski994@gmail.com>
2023-02-19 10:09:44 +0100

    finished task-06

---
+++ js/task-06.js
@@ -0,0 +1,18 @@
+const textInput = document.querySelector("#validation-input");
+console.log(textInput.getAttribute("data-length"));
+
+const getValidation = (event) => {
+  if (event.target.value.length >= textInput.getAttribute("data-length")) {
+    textInput.classList.add("valid");
+    if (textInput.classList.contains("invalid")) {
+      textInput.classList.remove("invalid");
+    }
+  } else {
+    if (textInput.classList.contains("valid")) {
+      textInput.classList.remove("valid");
+    }
+    textInput.classList.add("invalid");
+  }
+};
+
+textInput.addEventListener("blur", getValidation);
