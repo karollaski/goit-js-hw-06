8ae5fc0869461130c4f9292ffac44099f40b4147
Karol Łaski <karol.laski994@gmail.com>
2023-02-19 15:48:19 +0100

    finished task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeBtn = document.querySelector("button.change-color");
+const body = document.querySelector("body");
+const colorSelect = document.querySelector("span.color");
+
+const changeColor = (event) => {
+  body.style.backgroundColor = getRandomHexColor();
+  colorSelect.textContent = body.style.backgroundColor;
+};
+
+changeBtn.addEventListener("click", changeColor);
